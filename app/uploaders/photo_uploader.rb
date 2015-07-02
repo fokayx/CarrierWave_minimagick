f5b8cd0b4fe851d609016d8d73a19e6ab8cb8b8a
fokayx <fokayx@gmail.com>
2015-07-02 22:30:11 +0800

    Minimagick resize_adn_pad Watermark added => OK

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,7 +4,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -31,10 +31,27 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+#  process resize_to_fit: [800, 800]
+  process :watermark => "app/assets/images/watermark.png"
+
+#  process :resize_to_fill => [600, 600]
+   process :resize_and_pad => [600, 600, "white"]
+
+   def watermark(watermark)
+     manipulate! do |img|
+       img = img.composite(MiniMagick::Image.open(watermark, "jpg")) do |c|
+         c.gravity "SouthEast"
+       end
+       img = yield(img) if block_given?
+       img
+     end
+   end
+
+
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+   version :thumb do
+     process :resize_to_fit => [300, 300]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
